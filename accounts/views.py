1f5542abbe96290f47705ca2dd23ffb456860f35
arkr24 <you@example.com>
2026-06-02 22:50:56 +0530

    added bootstrap base template and messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-
+from django.contrib import messages
 
 def home(request):
     return render(request, 'accounts/home.html')
@@ -12,6 +12,12 @@ def register(request):
 
         if form.is_valid():
             form.save()
+
+            messages.success(
+                request,
+                "Account created successfully."
+            )
+
             return redirect('login')
 
     else:
