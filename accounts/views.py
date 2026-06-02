85e73afefd1e40cb5b8f913123fd08918705fdb6
arkr24 <you@example.com>
2026-06-02 22:33:48 +0530

    Implemented user registration and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
 
 def home(request):
     return render(request, 'accounts/home.html')
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    else:
+        form = RegisterForm()
+
+    return render(
+        request,
+        'accounts/register.html',
+        {'form': form}
+    )
