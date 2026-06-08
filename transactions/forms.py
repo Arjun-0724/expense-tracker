6d59c13793a66d33df83aa66d51c33b60257d193
arkr24 <you@example.com>
2026-06-08 18:19:01 +0530

    changed some UI

--- transactions/forms.py
+++ transactions/forms.py
@@ -2,26 +2,7 @@ from django import forms
 from .models import Transaction,Category
 from finance.models import Account
 
-
 class TransactionForm(forms.ModelForm):
-
-    def __init__(
-        self,
-        *args,
-        user=None,
-        **kwargs
-    ):
-        super().__init__(*args, **kwargs)
-
-        if user:
-
-            self.fields[
-                'account'
-            ].queryset = user.account_set.all()
-
-            self.fields[
-                'category'
-            ].queryset = user.category_set.all()
 
     class Meta:
         model = Transaction
@@ -32,5 +13,25 @@ class TransactionForm(forms.ModelForm):
             'transaction_type',
             'amount',
             'description',
-            'transaction_date',
+            'transaction_date'
         ]
+
+    def __init__(self, *args, user=None, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        if user:
+
+            self.fields[
+                'account'
+            ].queryset = Account.objects.filter(
+                user=user
+            )
+
+            self.fields[
+                'category'
+            ].queryset = self.fields[
+                'category'
+            ].queryset.filter(
+                user=user
+            )
